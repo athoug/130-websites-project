2fefee2996ad5e4029188a976178fe2cdb7b2a70
athoug <athoug@gwu.edu>
2016-03-30 17:41:57 +0300

    day 7

---
+++ day7/js/myScript.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//
+//     $('#ada').click(function() {
+//         var items = $('#gallery li');
+//
+//         items.each(function() {
+//             if(!($.hasClass( "one" ))) {
+//                 $.css({'display':'none'});
+//             }
+//         });
+//     });
+//
+//
+// });
+
+
+document.getElementById('ada').onclick = function(){
+
+};
