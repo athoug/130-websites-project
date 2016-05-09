f1a061b6101bc2f748454ce77096475cf1287c2a
athoug <athoug@gwu.edu>
2016-05-09 10:25:31 +0300

    day 15

---
+++ day15/js/script.js
@@ -0,0 +1,43 @@
+var endDate = '2016-05-25';
+
+
+
+function timeRemaining(endDate) {
+    var time = Date.parse(endDate) - Date.parse(new Date());
+    var seconds = Math.floor((time/1000)%60);
+    var minutes = Math.floor((time/1000/60)%60);
+    var hours = Math.floor((time/(1000*60*60))%24);
+    var days = Math.floor(time/(1000*60*60*24));
+
+    console.log(time);
+
+    return {
+        'total': time,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(endDate){
+    var theDay = document.getElementById('day');
+    var theHours = document.getElementById('hours');
+    var theMinutes = document.getElementById('minutes');
+    var theSecongs = document.getElementById('seconds');
+
+    var timeinerval = setInterval(function(){
+        var t = timeRemaining(endDate);
+        theDay.innerHTML = t.days;
+        theHours.innerHTML = t.hours;
+        theMinutes.innerHTML = t.minutes;
+        theSecongs.innerHTML = t.seconds;
+
+        if(t.total <=0){
+            clearInterval(timeinerval);
+        }
+    },1000);
+}
+
+
+initializeClock(endDate);
