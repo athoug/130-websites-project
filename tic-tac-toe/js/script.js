87d649dcdbacf73faec5777209195256cf8283ea
Athoug Alsoughayer <athoug.s@gmail.com>
2017-04-04 15:47:02 -0400

    update tic tac toe UI

--- tic-tac-toe/js/script.js
+++ tic-tac-toe/js/script.js
@@ -1,4 +1,24 @@
 window.onload = function(){
+var turn = 1;
 
-  
+var grid = document.getElementsByClassName('box');
+
+var drawTurn = function(){
+  if(this.textContent){
+    return null;
+  }
+  if(turn%2 == 0){
+    this.innerHTML += "O";
+    this.className += " os";
+  } else{
+    this.innerHTML += "X";
+    this.className += " xs";
+  }
+  turn += 1;
+};
+
+for(var i = 0; i < grid.length; i++){
+    grid[i].addEventListener("click", drawTurn, false);
+}
+
 }
