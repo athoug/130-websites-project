61cb0741bb72abf2c5d01e9ed829ec65e47fddf8
Athoug Alsoughayer <athoug.s@gmail.com>
2017-05-25 19:06:07 -0400

    Update style

--- day47/js/script.js
+++ day47/js/script.js
@@ -4,6 +4,9 @@ window.onload = function() {
   const secondHand = document.querySelector('.second-hand');
   const minuteHand = document.querySelector('.min-hand');
   const hourHand = document.querySelector('.hour-hand');
+  const date = document.querySelector('.date');
+
+  date.textContent = new Date().getDate();
 
   function setDate() {
     const now = new Date();
