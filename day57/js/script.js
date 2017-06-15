b7b47784941dd54e79338444f72d92cba27961ca
Athoug Alsoughayer <athoug.s@gmail.com>
2017-06-15 01:50:19 -0400

    day 57

---
+++ day57/js/script.js
@@ -0,0 +1,50 @@
+particlesJS("space", {
+  "particles": {
+    "number": {
+      "value": 60,
+      "density": {
+        "enable": true,
+        "value_area": 500
+      }
+    },
+    "color": {
+      "value": "#FBF7E7"
+    },
+    "opacity": {
+      "value": 1,
+      "anim": {
+        "enable": true,
+        "speed": 8,
+        "opacity_min": 0.4,
+        "sync": false
+      }
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "value": 5,
+      "random": true
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "right",
+      "straight": true
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      },
+      "onclick": {
+        "enable": false
+      }
+    }
+  }
+});
