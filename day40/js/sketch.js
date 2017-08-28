ccc40a6b1e9c788d52fa947e74fa00bcfac7d8f1
Athoug Alsoughayer <athoug.s@gmail.com>
2017-08-28 09:45:08 +0300

    Update site design

--- day40/js/sketch.js
+++ day40/js/sketch.js
@@ -1,29 +1,53 @@
-var sizeof = 20;
+var circles = [];
 var canvas = document.getElementsByTagName('canvas')[0];
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  background("#130c22");
+}
+
+
+function draw() {
+
+  // craete a variable that holds the circle object
+  var cir = new Circle(mouseX, mouseY);
+
+  // add variable to array
+  circles.push(cir);
+
+  // updated cricle positions
+  for (var i = 0; i < circles.length; i++) {
+      circles[i].drawCircle();
+      circles[i].updateCircle();
+  }
+
+  // uupdate the div background
+  assignDivBackground();
+}
+
+// circle consturctor
+function Circle(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = "#372261"; //'rgb(51,136,255)';
+    // this.alpha = random(0.8, 2);
+    this.size = random(5, 50);
+
+    this.drawCircle = function () {
+        // draw circles
+        noFill()
+        stroke(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+    }
+    this.updateCircle = function () {
+        var offset = random(-1, 1);
+        this.x += offset;
+        this.y += offset;
+    }
+}
 
 // assign the canvas as background
 function assignDivBackground() {
     dataUrl = canvas.toDataURL();
     document.getElementById('bg').style.background = 'url('+dataUrl+')';
-}
-
-
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  background("#5540FF");
-}
-
-
-
-function draw() {
-
-  // draw circles
-  noFill()
-  stroke("#3388FF");
-  ellipse(mouseX, mouseY, sizeof, sizeof);
-
-  // update size
-  sizeof = random(5, 50);
-
-  assignDivBackground();
 }
