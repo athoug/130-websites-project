abdbdf03a969689fe9b572252b601585576f0200
Athoug Alsoughayer <athoug2.s@gmail.com>
2017-11-28 14:15:48 +0300

    day61

--- day61/js/script.js
+++ day61/js/script.js
@@ -1,1 +1,8 @@
-alert("Am I connected?");
+function playString(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return; // if the ausio doesn't exist then stop the function
+  audio.currentTime = 0; // reset the sound
+  audio.play();
+}
+
+window.addEventListener("keydown", playString);
