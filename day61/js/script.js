b721f1cf4f100786a4a2223f889cf271b8e261d6
Athoug Alsoughayer <athoug2.s@gmail.com>
2017-11-29 14:21:52 +0300

    adjustments
    - add click event to strings
    - fix viewport for mobile
    - add string highlight

--- day61/js/script.js
+++ day61/js/script.js
@@ -1,8 +1,19 @@
 function playString(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return; // if the ausio doesn't exist then stop the function
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) {
+    audio = document.querySelector(`audio[data-key="${this.getAttribute("data-key")}"]`);
+    if(!audio) return; // if the ausio doesn't exist then stop the function
+  }
   audio.currentTime = 0; // reset the sound
   audio.play();
 }
 
+// adding the keydown event listener to the whole page
 window.addEventListener("keydown", playString);
+
+// adding the click event listers to the strings
+// grab the string classes
+var strings = document.querySelectorAll(".strings");
+console.log(strings);
+
+strings.forEach(str => str.addEventListener("click", playString));
