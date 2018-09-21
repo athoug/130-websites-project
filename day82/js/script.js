9ef6ec44337826056b2590018363c9f50d50bb92
Athoug Alsoughayer <athoug.s@gmail.com>
2018-09-21 16:58:45 +0300

    add marker

--- day82/js/script.js
+++ day82/js/script.js
@@ -1,8 +1,13 @@
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var today = new Date();
-
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let idNmae = 'day-';
+let today = new Date();
+let marker = document.createElement('div');
+marker.setAttribute('class', 'active');
+    
 document.getElementById('today').textContent = today.getDate();
 
 document.getElementById('month').textContent = months[today.getMonth()];
 
 document.getElementById('year').textContent = today.getFullYear();
+
+document.getElementById(idNmae + today.getDate()).appendChild(marker);
