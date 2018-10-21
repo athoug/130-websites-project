4a8fafa1082536914d06ba5911cb233684403c6f
Athoug Alsoughayer <athoug.s@gmail.com>
2018-10-21 11:10:36 +0300

    keyboard working

--- day112/js/script.js
+++ day112/js/script.js
@@ -1,9 +1,19 @@
 const keyboard = Array.from(document.querySelectorAll(".key"));
 
 keyboard.forEach(key => key.addEventListener("click", playAudio));
+window.addEventListener("keydown", playAudio);
 
 
 function playAudio(e) {
-    let audio = document.querySelector(`audio[data-key=${e.keycode}]`);
-    console.log(audio);
+    let audio = document.querySelector(`audio[data-key=${e.key}]`);
+    let key = document.querySelector(`div[data-key=${e.key}]`);
+    console.log(e);
+    if(!audio) {
+        audio = document.querySelector(`audio[data-key=${this.getAttribute("data-key")}]`);
+        key = document.querySelector(`div[data-key=${this.getAttribute("data-key")}]`);
+        if (!audio) return;
+    }
+
+    audio.currentTime = 0;
+    audio.play();
 }
