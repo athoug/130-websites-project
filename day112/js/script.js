71971f41aa10b1a9ff13f7352cfa24c7ea823d6d
Athoug Alsoughayer <athoug.s@gmail.com>
2018-10-21 11:19:40 +0300

    tranistion

--- day112/js/script.js
+++ day112/js/script.js
@@ -3,6 +3,9 @@ const keyboard = Array.from(document.querySelectorAll(".key"));
 keyboard.forEach(key => key.addEventListener("click", playAudio));
 window.addEventListener("keydown", playAudio);
 
+// adding UX with key presses
+keyboard.forEach(key => key.addEventListener('transitionend', removeTransition));
+
 
 function playAudio(e) {
     let audio = document.querySelector(`audio[data-key=${e.key}]`);
@@ -14,6 +17,20 @@ function playAudio(e) {
         if (!audio) return;
     }
 
+    // add effect for key being pressed
+      if(key.classList.contains('keys-white')) {
+        key.classList.add('white-keys-clicked');
+      } else if (key.classList.contains('keys-black')) {
+        key.classList.add('black-keys-clicked');
+      }
+
     audio.currentTime = 0;
     audio.play();
+}
+
+function removeTransition(e) {
+    console.log(e.propertyName);
+    if (e.propertyName !== 'background-color') return;
+    e.target.classList.remove('white-keys-clicked');
+    e.target.classList.remove('black-keys-clicked');
 }
