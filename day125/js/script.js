ef75cb548bed89f27ee103202dc4267cb87b9923
Athoug Alsoughayer <athoug2.s@gmail.com>
2018-11-06 13:49:05 +0300

    completed script

--- day125/js/script.js
+++ day125/js/script.js
@@ -1,18 +1,24 @@
-const secondsHand = document.querySelector('.seconds-hand');
-const minuteHand = document.querySelector('.minutes-hand');
-const hourHand = document.querySelector('.hours-hand');
+const secondsHand = document.querySelectorAll('.seconds-hand');
+const minuteHand = document.querySelectorAll('.minutes-hand');
+const hourHand = document.querySelectorAll('.hours-hand');
 
-function setTime(itteration) {
+function setTime(itteration, type, element) {
     const now = new Date();
 
     const seconds = ((now.getSeconds() / 60) * 360) + 90;
-    const minutes = ((now.getMinutes() / 60) * 360) + 90;
-    const hours = ((now.getHours() / 12) * 360) + 90;
+    const minutes = (((now.getMinutes() + itteration) / 60) * 360) + 90;
+    const hours = (((now.getHours() + itteration) / 12) * 360) + 90;
 
-    secondsHand.style.transform = `rotate(${seconds}deg)`;
-    minuteHand.style.transform = `rotate(${minutes}deg)`;
-    hourHand.style.transform = `rotate(${hours}deg)`;
+    if (type === 's') element.style.transform = `rotate(${seconds}deg)`;
+    if (type === 'm') element.style.transform = `rotate(${minutes}deg)`;
+    if (type === 'h') element.style.transform = `rotate(${hours}deg)`;
 
 }
 
-setInterval(setTime, 1000);
+for (let index = 0; index < secondsHand.length; index++) {
+    setInterval(function (params) {
+        setTime(index, 's', secondsHand[index]);
+        setTime(index, 'm', minuteHand[index]);
+        setTime(index, 'h', hourHand[index]);
+    }, 1000);
+}
