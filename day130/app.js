0f51f7490d3fb6d1d166b311a52d7d701485df67
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-04-07 13:15:18 +0300

    controlls added

--- day130/app.js
+++ day130/app.js
@@ -1,31 +1,65 @@
-let selectedFeature = undefined;
+// --- Variables ---
+let selectedFeature;
 
-// DOM elements selecting
 const face = document.querySelector('.face');
+const inputsRange = document.querySelectorAll('.controls input[type="range"]');
+const inputCheckBox = document.querySelectorAll('.controls input[type="checkbox"]');
+const inputRadio = document.querySelectorAll('.controls input[type="radio"]');
+
+// --- METHODS ---
+// function to set up the pointer element for color changes
+function setPointerElement(e) {
+  // make sure the element isn't the container
+  if ([...e.target.classList].includes('container')) return; // exit of it's the container
+  selectedFeature = e.target; // set the selector to the target element
+}
+
+// function to change the color of the element
+function colorMe() {
+  // 1. get the color value
+  const color = this.value;
+  // 2. get the selected element if no element was selected, color the skin
+  const selected = selectedFeature || face;
+  // 3. re-assign the css variable based on teh input value
+  document.documentElement.style.setProperty(`--${selected.dataset.key}-color`, color);
+}
+
+// function to change the faces dismentions
+function changeDismentions() {
+  const suffix = this.dataset.suffix || '';
+  // get the changed value [this.value] | get the name [this.name] of the variable to be changed
+  document.documentElement.style.setProperty(`--${this.name}`, `${this.value}${suffix}`);
+}
+
+// function to handel checkbox events
+function checkBoxHandler() {
+  const display = !this.checked ? 'none' : 'display';
+  document.documentElement.style.setProperty(`--${this.name}`, display);
+}
+
+// function to handle radio buttons
+function radioButtonHandler() {
+  if (this.value === 'full') {
+    document.documentElement.style.setProperty(`--${this.name}`, 'display');
+    document.documentElement.style.setProperty(`--${this.name}-front`, 'display');
+  } else if (this.value === 'bold') {
+    document.documentElement.style.setProperty(`--${this.name}-front`, 'none');
+    document.documentElement.style.setProperty(`--${this.name}`, 'none');
+  } else {
+    document.documentElement.style.setProperty(`--${this.name}-front`, 'none');
+    document.documentElement.style.setProperty(`--${this.name}`, 'display');
+  }
+}
 
 // add lisner to container, and record the pointer
-document.querySelector('.container').addEventListener('click', setPointerElement)
+document.querySelector('.container').addEventListener('click', setPointerElement);
 
 // select the color input and add a listner
 const inputColor = document.querySelector('.controls input[type="color"]');
 inputColor.addEventListener('change', colorMe);
 
+inputsRange.forEach(input => input.addEventListener('change', changeDismentions));
 
-// --- METHODS ---
+inputCheckBox.forEach(input => input.addEventListener('click', checkBoxHandler));
 
-// function to set up the pointer element for color changes
-function setPointerElement(e) {
-    // make sure the element isn't the container
-    if ([...e.target.classList].includes('container')) return; // exit of it's the container
-    selectedFeature = e.target; // set the selector to the target element
-}
-
-// function to change the color of the element
-function colorMe(e) {
-    // 1. get the color value
-    const color = this.value;
-    // 2. get the selected element if no element was selected, color the skin
-    const selected = selectedFeature || face;
-    // 3. re-assign the css variable based on teh input value
-    document.documentElement.style.setProperty(`--${selected.dataset.key}-color`, color);
-}
+inputRadio.forEach(input => input.addEventListener('click', radioButtonHandler));
--- day130/readme.md
+++ day130/readme.md
@@ -75,7 +75,7 @@ and what we get is this [please ignore the purple border I'm using it for testin
 
 okay let me firt show the relationship between the components
 
-```
+``` txt
     container [parent]
         hair [child of container | siblin of bellow elements]
         ear left [child of container | siblin of bellow elements]
@@ -282,3 +282,41 @@ and with those rule sets we get this lovely chap
 now lets move on to the functionality
 
 ### Javascript
+
+I wanted to start off with the color. For this I wanted to have an initial element that changes and when the user clicks on an element, They have the option of chnaging that elements color. My approach was as such:
+
+1. Create a pointed variable that isn't defined.
+2. Attach an event listner to the container that assigns the variable to the pointed element
+3. then select the color input and attach an event listner to it and if no element is selected have the defult face to be changed
+4. change the css variable for that elements color
+
+with the steps outlined, my JS code looks as such
+
+``` javascript
+let selectedFeature;
+const face = document.querySelector('.face');
+
+// function to set up the pointer element for color changes
+function setPointerElement(e) {
+  // make sure the element isn't the container
+  if ([...e.target.classList].includes('container')) return; // exit of it's the container
+  selectedFeature = e.target; // set the selector to the target element
+}
+
+// function to change the color of the element
+function colorMe() {
+  // 1. get the color value
+  const color = this.value;
+  // 2. get the selected element if no element was selected, color the skin
+  const selected = selectedFeature || face;
+  // 3. re-assign the css variable based on teh input value
+  document.documentElement.style.setProperty(`--${selected.dataset.key}-color`, color);
+}
+
+// add lisner to container, and record the pointer
+document.querySelector('.container').addEventListener('click', setPointerElement);
+
+// select the color input and add a listner
+const inputColor = document.querySelector('.controls input[type="color"]');
+inputColor.addEventListener('change', colorMe);
+```
