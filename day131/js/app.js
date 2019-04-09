943dd20107f086bf932ddb0054762c40fd423860
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-04-09 13:38:00 +0300

    dynamic data

--- day131/js/app.js
+++ day131/js/app.js
@@ -1,1 +1,35 @@
+// -- Functions --
+function createCards(array, parentElement, icons) {
+  // 1. create the container fragment
+  const fragment = document.createDocumentFragment();
+  // 2. loop over each element in the array and construct the html element and append it
+  array.forEach(item => {
+    const div = document.createElement('div');
+    div.className = "scene";
+    div.innerHTML = `
+      <div class="card">
+        <div class="card-face face-front">
+          <p>${item.name}</p>
+          <div class="icon">
+            <img src="${icons[item.genre]}" alt="${item.genre} genere" class="icon-img">
+            <span>${item.genre}</span>
+          </div>
+        </div>
+        <div class="card-face face-back">
+          <p>${item.disc}</p>
+          <div class="icon">
+            <img src="./svg/website.svg" alt="website" class="icon-img">
+            <a href="${item.link}">${item.name}</a>
+          </div>
+        </div>
+      </div>
+    `;
+    fragment.appendChild(div);
+  });
+  // 3. add the fragment to the parent element
+  parentElement.appendChild(fragment);
+}
+
 const container = document.querySelector('.container');
+
+createCards(data, container, icon);
--- day131/readme.md
+++ day131/readme.md
@@ -7,3 +7,13 @@ so lets get started
 ## Walkthrough
 
 This time around, I wanted to right off the bat make the application responsive instread of leaving it as a last step so lets get started on that.
+
+## Resources
+
+A list of resources that helped me throughout this project
+
+* [Pure CSS card flip](https://codepen.io/edeesims/pen/iGDzk?editors=1100)
+* [introduction to CSS 3D transforms](https://3dtransforms.desandro.com/)
+* [MDN: Transform](https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function)
+* [MDN: document Fragment](https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment)
+* [Stackoverflow: Add class to node crated element](https://stackoverflow.com/questions/42831008/how-to-add-class-to-element-created-using-document-createelement)
